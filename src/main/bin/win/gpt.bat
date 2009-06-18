ebca6df729c7843088b2155b5ce7a209762178a9
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-06-18 17:13:56 +0000

    add batch,shell scripts for grid cell exporter, adapt assembly descriptor
    git-svn-id: https://www.brockmann-consult.de/svn/os/smos-box/trunk@5693 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ src/main/bin/win/gpt.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set BEAM4_HOME=${installer:sys.installationDir}
+
+"%BEAM4_HOME%\jre\bin\java.exe" ^
+    -Xmx1024M ^
+    -Dceres.context=beam ^
+    "-Dbeam.mainClass=org.esa.beam.smos.visat.export.GridCellExport" ^
+    "-Dbeam.home=%BEAM4_HOME%" ^
+    -jar "%BEAM4_HOME%\bin\ceres-launcher.jar" %*
+
+exit /B 0
