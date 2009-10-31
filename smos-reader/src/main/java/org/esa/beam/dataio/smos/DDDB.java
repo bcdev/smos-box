5e1d81ce6ff2eac6a7d7dcff04e05d0eab47bb25
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2009-10-31 11:30:45 +0000

    moved resource path builder to inner class
    git-svn-id: https://www.brockmann-consult.de/svn/os/smos-box/trunk@6804 d90d2d48-2b33-0410-a973-e72f85796a02

--- smos-reader/src/main/java/org/esa/beam/dataio/smos/DDDB.java
+++ smos-reader/src/main/java/org/esa/beam/dataio/smos/DDDB.java
@@ -469,4 +469,18 @@ public class DDDB {
             return token.trim();
         }
     }
+
+    static class ResourcePathBuilder {
+
+        String buildPath(String identifier, String root, String appendix) {
+            final String fc = identifier.substring(12, 16);
+            final String sd = identifier.substring(16, 22);
+
+            final StringBuilder pathBuilder = new StringBuilder();
+            pathBuilder.append(root).append("/").append(fc).append("/").append(sd).append("/").append(identifier);
+            pathBuilder.append(appendix);
+
+            return pathBuilder.toString();
+        }
+    }
 }
