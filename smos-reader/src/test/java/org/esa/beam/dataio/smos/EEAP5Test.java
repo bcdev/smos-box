49be9cd68d700ff99a0948c0e7b44059793057a1
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2010-01-14 15:57:10 +0000

    EEAP5 test
    git-svn-id: https://www.brockmann-consult.de/svn/os/smos-box/trunk@8002 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ smos-reader/src/test/java/org/esa/beam/dataio/smos/EEAP5Test.java
@@ -0,0 +1,13 @@
+package org.esa.beam.dataio.smos;
+
+import org.junit.Before;
+
+public class EEAP5Test {
+
+    private EEAP5 eeap5;
+
+    @Before
+    public void setup() {
+        eeap5 = new EEAP5();
+    }
+}
