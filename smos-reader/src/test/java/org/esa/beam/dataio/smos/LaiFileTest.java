439465c06eaad6a276679def7a3b51bd074d40cc
ralf <ralf@d90d2d48-2b33-0410-a973-e72f85796a02>
2010-01-15 14:41:36 +0000

    LAI file test
    git-svn-id: https://www.brockmann-consult.de/svn/os/smos-box/trunk@8017 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ smos-reader/src/test/java/org/esa/beam/dataio/smos/LaiFileTest.java
@@ -0,0 +1,103 @@
+package org.esa.beam.dataio.smos;
+
+import com.bc.ceres.binio.CompoundData;
+import com.bc.ceres.binio.CompoundType;
+import com.bc.ceres.binio.DataFormat;
+import com.bc.ceres.binio.SequenceData;
+import com.bc.ceres.binio.SequenceType;
+import com.bc.ceres.binio.Type;
+import com.bc.ceres.binio.util.DataPrinter;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LaiFileTest {
+
+    private static final File USER_HOME = new File(System.getProperty("user.home"));
+    private static final File DATA_DIR = new File(USER_HOME,
+                                                  "SM_OPER_AUX_DFFLAI_20091219T000000_20100118T000000_306_001_3");
+    private static final File DBL_FILE = new File(DATA_DIR,
+                                                  "SM_OPER_AUX_DFFLAI_20091219T000000_20100118T000000_306_001_3.DBL");
+    private static final File HDR_FILE = new File(DATA_DIR,
+                                                  "SM_OPER_AUX_DFFLAI_20091219T000000_20100118T000000_306_001_3.HDR");
+
+    private static final String DFFG_LAI_NAME = "DFFG_LAI";
+    private static final int ZONE_COUNT = 74;
+
+    @Test
+    public void format() throws IOException {
+        if (HDR_FILE.exists()) {
+            final DataFormat dataFormat = DDDB.getInstance().getDataFormat(HDR_FILE);
+            final CompoundType dataBlockType = dataFormat.getType();
+
+            assertEquals(1, dataBlockType.getMemberCount());
+            assertEquals(0, dataBlockType.getMemberIndex(DFFG_LAI_NAME));
+
+            final Type zoneSequenceDataType = dataBlockType.getMember(0).getType();
+            assertTrue(zoneSequenceDataType.isSequenceType());
+
+            final SequenceType sequenceType = (SequenceType) zoneSequenceDataType;
+            assertEquals(ZONE_COUNT, sequenceType.getElementCount());
+        }
+    }
+
+    @Test
+    public void zoneDataSizes() throws IOException {
+        if (HDR_FILE.exists() && DBL_FILE.exists()) {
+            final DataFormat dataFormat = DDDB.getInstance().getDataFormat(HDR_FILE);
+            final ExplorerFile explorerFile = new LaiFile(HDR_FILE, DBL_FILE, dataFormat);
+
+            final SequenceData sequenceData = explorerFile.getDataBlock().getSequence(DFFG_LAI_NAME);
+            assertEquals(ZONE_COUNT, sequenceData.getElementCount());
+
+            CompoundData compoundData;
+
+            compoundData = sequenceData.getCompound(0);
+            assertEquals(0, compoundData.getPosition());
+            compoundData.resolveSize();
+            assertEquals(3791314, compoundData.getSize());
+
+            compoundData = sequenceData.getCompound(1);
+            assertEquals(3791314, compoundData.getPosition());
+            compoundData.resolveSize();
+            assertEquals(3791314, compoundData.getSize());
+
+            compoundData = sequenceData.getCompound(2);
+            compoundData.resolveSize();
+            assertEquals(3052488, compoundData.getSize());
+
+            compoundData = sequenceData.getCompound(3);
+            compoundData.resolveSize();
+            assertEquals(3052488, compoundData.getSize());
+        }
+    }
+
+    // for dumping the contents of a LAI file
+    public static void main(String[] args) throws IOException {
+        final DataFormat dataFormat = DDDB.getInstance().getDataFormat(HDR_FILE);
+        final ExplorerFile explorerFile = new LaiFile(HDR_FILE, DBL_FILE, dataFormat);
+        final SequenceData sequenceData = explorerFile.getDataBlock().getSequence(DFFG_LAI_NAME);
+
+        try {
+            for (int i = sequenceData.getElementCount(); i-- > 0;) {
+                final int zoneId = sequenceData.getCompound(i).getInt("Zone_ID");
+                final String fileName = DBL_FILE.getName().replace("DBL", zoneId + ".TXT");
+                final File file = new File(DATA_DIR, fileName);
+                final PrintStream printStream = new PrintStream(file);
+                final DataPrinter dataPrinter = new DataPrinter(printStream, false);
+                try {
+                    dataPrinter.print(sequenceData.getCompound(i));
+                } finally {
+                    printStream.close();
+                }
+            }
+        } finally {
+            explorerFile.close();
+        }
+    }
+}
