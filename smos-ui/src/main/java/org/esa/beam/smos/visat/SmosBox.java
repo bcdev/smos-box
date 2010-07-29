a1874f450cb3506ced253c54b7808ce40f02a165
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2010-07-29 19:39:39 +0200

    - update copyright - change dependency to BEAM 4.8, ceres 0.11 - replace usage of blue marble with configured world map type

--- smos-ui/src/main/java/org/esa/beam/smos/visat/SmosBox.java
+++ smos-ui/src/main/java/org/esa/beam/smos/visat/SmosBox.java
@@ -1,20 +1,41 @@
+/*
+ * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
 package org.esa.beam.smos.visat;
 
 import com.bc.ceres.binding.PropertySet;
 import com.bc.ceres.glayer.Layer;
+import com.bc.ceres.glayer.LayerFilter;
 import com.bc.ceres.glayer.LayerType;
 import com.bc.ceres.glayer.LayerTypeRegistry;
+import com.bc.ceres.glayer.support.LayerUtils;
 import org.esa.beam.dataio.smos.ExplorerFile;
 import org.esa.beam.dataio.smos.L1cScienceSmosFile;
 import org.esa.beam.dataio.smos.SmosProductReader;
 import org.esa.beam.framework.dataio.ProductReader;
 import org.esa.beam.framework.datamodel.RasterDataNode;
 import org.esa.beam.framework.ui.product.ProductSceneView;
+import org.esa.beam.glayer.WorldMapLayerType;
 import org.esa.beam.visat.VisatApp;
 import org.esa.beam.visat.VisatPlugIn;
-import org.esa.beam.worldmap.BlueMarbleLayerType;
 
 public class SmosBox implements VisatPlugIn {
+
+    private static final String WORLDMAP_TYPE_PROPERTY_NAME = "worldmap.type";
+    private static final String BLUE_MARBLE_LAYER_TYPE = "BlueMarbleLayerType";
 
     private static volatile SmosBox instance;
 
@@ -50,16 +71,13 @@ public class SmosBox implements VisatPlugIn {
                 @Override
                 public void handleSceneViewSelectionChanged(ProductSceneView oldView, ProductSceneView newView) {
                     if (newView != null) {
-                        final Layer rootLayer = newView.getRootLayer();
-                        final LayerType layerType = LayerTypeRegistry.getLayerType(BlueMarbleLayerType.class.getName());
-                        if (!hasLayer(rootLayer, layerType)) {
-                            final PropertySet configuration = layerType.createLayerConfig(null);
-                            final Layer worldMapLayer = layerType.createLayer(null, configuration);
-                            if (worldMapLayer != null) {
-                                rootLayer.getChildren().add(worldMapLayer);
-                                worldMapLayer.setVisible(true);
-                            }
+                        Layer worldMapLayer = findWorldMapLayer(newView);
+                        if (worldMapLayer == null) {
+                            worldMapLayer = createWorldMapLayer();
+                            final Layer rootLayer = newView.getRootLayer();
+                            rootLayer.getChildren().add(worldMapLayer);
                         }
+                        worldMapLayer.setVisible(true);
                     }
                 }
             });
@@ -101,7 +119,6 @@ public class SmosBox implements VisatPlugIn {
                 }
             }
         }
-
         return null;
     }
 
@@ -116,12 +133,25 @@ public class SmosBox implements VisatPlugIn {
         return null;
     }
 
-    public static boolean hasLayer(Layer parentLayer, LayerType layerType) {
-        for (final Layer childLayer : parentLayer.getChildren()) {
-            if (layerType == childLayer.getLayerType()) {
-                return true;
+    private LayerType getWorldMapLayerType() {
+        final VisatApp visatApp = VisatApp.getApp();
+        String layerTypeClassName = visatApp.getPreferences().getPropertyString(WORLDMAP_TYPE_PROPERTY_NAME,
+                                                                                BLUE_MARBLE_LAYER_TYPE);
+        return LayerTypeRegistry.getLayerType(layerTypeClassName);
+    }
+
+    private Layer createWorldMapLayer() {
+        final LayerType layerType = getWorldMapLayerType();
+        final PropertySet template = layerType.createLayerConfig(null);
+        return layerType.createLayer(null, template);
+    }
+
+    private Layer findWorldMapLayer(ProductSceneView view) {
+        return LayerUtils.getChildLayer(view.getRootLayer(), LayerUtils.SearchMode.DEEP, new LayerFilter() {
+            @Override
+            public boolean accept(Layer layer) {
+                return layer.getLayerType() instanceof WorldMapLayerType;
             }
-        }
-        return false;
+        });
     }
 }
