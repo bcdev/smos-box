afa2698d2b1bf9c1660aaa51e13b697be1f37f8f
Tom Block <tom.block@brockmann-consult.de>
2011-12-12 16:19:09 +0100

    added dummy classes

---
+++ smos-tools/src/test/java/org/esa/beam/smos/util/SmosUtilsTest.java
@@ -0,0 +1,9 @@
+package org.esa.beam.smos.util;
+
+import junit.framework.TestCase;
+
+public class SmosUtilsTest extends TestCase {
+    public void testDUMMY() {
+        // @todo 1 tb/** replece with more meaningful test :-)
+    }
+}
