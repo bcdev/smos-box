080e4c5076f8c5e2fb121ff0f0e414b5a3264853
Norbert Haar <Norbert.Haar@Brockmann-Consult.de>
2012-04-11 18:02:31 +0200

    process() now return the amount of intersecting grid points

--- smos-ui/src/main/java/org/esa/beam/smos/visat/export/SmosFileProcessor.java
+++ smos-ui/src/main/java/org/esa/beam/smos/visat/export/SmosFileProcessor.java
@@ -35,7 +35,7 @@ public class SmosFileProcessor {
         this.gridPointFilter = gridPointFilter;
     }
 
-    public void process(SmosFile smosFile, ProgressMonitor pm) throws IOException {
+    public long process(SmosFile smosFile, ProgressMonitor pm) throws IOException {
         final CompoundType gridPointType = smosFile.getGridPointType();
         final int idIndex = gridPointType.getMemberIndex(SmosConstants.GRID_POINT_ID_NAME);
 
@@ -44,12 +44,14 @@ public class SmosFileProcessor {
 
         pm.beginTask(MessageFormat.format(
                 "Processing file ''{0}''...", smosFile.getDblFile().getName()), gridPointCount);
+        long intersectingGridPointsCounter = 0;
         try {
             for (int i = 0; i < gridPointCount; i++) {
                 final CompoundData gridPointData = smosFile.getGridPointData(i);
                 final int id = gridPointData.getInt(idIndex);
                 if (gridPointFilter.accept(id, gridPointData)) {
                     filterStream.handleGridPoint(i, gridPointData);
+                    intersectingGridPointsCounter++;
                 }
                 pm.worked(1);
                 if (pm.isCanceled()) {
@@ -60,5 +62,6 @@ public class SmosFileProcessor {
             filterStream.stopFile(smosFile);
             pm.done();
         }
+        return intersectingGridPointsCounter;
     }
 }
