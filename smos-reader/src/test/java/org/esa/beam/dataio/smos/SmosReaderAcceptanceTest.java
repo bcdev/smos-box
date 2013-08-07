fde8ce28d5ce5604f5eddf9950a25ad8fdb26b7d
marpet <marco.peters@brockmann-consult.de>
2013-08-07 12:11:42 +0200

    - added new BEAM reader acceptance test - removed old one and its data - increased version and dependencies

---
+++ smos-reader/src/test/java/org/esa/beam/dataio/smos/SmosReaderAcceptanceTest.java
@@ -0,0 +1,12 @@
+package org.esa.beam.dataio.smos;
+
+import org.esa.beam.dataio.ProductReaderAcceptanceTest;
+
+
+/**
+ * @author Marco Peters
+ */
+public class SmosReaderAcceptanceTest extends ProductReaderAcceptanceTest {
+
+
+}
