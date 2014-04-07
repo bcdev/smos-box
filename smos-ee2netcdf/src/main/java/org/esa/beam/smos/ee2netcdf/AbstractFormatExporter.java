487e33b4cd2b8ab08df252f2d55520fda0a752da
TomBlock <tom.block@brockmann-consult.de>
2014-04-07 13:53:34 +0200

    extracted abstract base class

---
+++ smos-ee2netcdf/src/main/java/org/esa/beam/smos/ee2netcdf/AbstractFormatExporter.java
@@ -0,0 +1,36 @@
+package org.esa.beam.smos.ee2netcdf;
+
+
+import org.esa.beam.dataio.netcdf.nc.NFileWriteable;
+import org.esa.beam.dataio.smos.SmosFile;
+import org.esa.beam.dataio.smos.SmosProductReader;
+import org.esa.beam.dataio.smos.util.DateTimeUtils;
+import org.esa.beam.framework.datamodel.Product;
+
+import java.io.IOException;
+import java.util.Date;
+
+abstract class AbstractFormatExporter implements FormatExporter {
+
+    protected int gridPointCount;
+
+    @Override
+    public void initialize(Product product) {
+        final SmosProductReader smosReader = (SmosProductReader) product.getProductReader();
+        final SmosFile explorerFile = (SmosFile) smosReader.getExplorerFile();
+        gridPointCount = explorerFile.getGridPointCount();
+    }
+
+    @Override
+    public  void addGlobalAttributes(NFileWriteable nFileWriteable) throws IOException {
+        nFileWriteable.addGlobalAttribute("Conventions", "CF-1.6");
+        nFileWriteable.addGlobalAttribute("title", "TBD");  // @todo 2 tb/tb replace with meaningful value tb 2014-04-07
+        nFileWriteable.addGlobalAttribute("institution", "TBD");  // @todo 2 tb/tb replace with meaningful value tb 2014-04-07
+        nFileWriteable.addGlobalAttribute("contact", "TBD");  // @todo 2 tb/tb replace with meaningful value tb 2014-04-07
+        nFileWriteable.addGlobalAttribute("creation_date", DateTimeUtils.toFixedHeaderFormat(new Date()));
+        nFileWriteable.addGlobalAttribute("total_number_of_grid_points", Integer.toString(gridPointCount));
+    }
+
+    @Override
+    abstract public void addDimensions(NFileWriteable nFileWriteable) throws IOException;
+}
